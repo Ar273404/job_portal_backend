ff8d9ea853414f557449ac48f6d5063e8d05034b
Ar273404 <arun273404@gmail.com>
2024-07-16 11:05:32 +0530

    changing

--- app.js
+++ app.js
@@ -1,53 +1,41 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import fileUpload from "express-fileupload";
-import userRouter from "./routes/userRouter.js";
-import jobRouter from "./routes/jobRouter.js";
-import applicationRouter from "./routes/applicationRouter.js";
-import { db } from "./database/db.js";
-import { errorMiddleware } from "./middlewares/error.js";
-import path from "path";
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import fileUpload from 'express-fileupload';
+import userRouter from './routes/userRouter.js'
+import jobRouter from './routes/jobRouter.js'
+import applicationRouter from './routes/applicationRouter.js'
+import { db } from './database/db.js';
+import { errorMiddleware } from './middlewares/error.js';
+
 
 const app = express();
 
-dotenv.config({ path: "./config/config.env" });
+dotenv.config({path:'./config/config.env'});
 
-// Serve static files from the 'static' directory
-app.use(express.static(path.join(process.cwd(), "static")));
-
-// Define a route for '/'
-app.get("/", (req, res) => {
-  res.send("Hello, World!"); // Respond with a simple message
-});
-
-app.use(
-  cors({
-    origin: [process.env.FRONTED_URL],
-    methods: ["GET", "POST", "DELETE", "PUT"],
-    credentials: true,
-  })
-);
+app.use(cors({
+    origin:[process.env.FRONTED_URL],
+    methods:['GET','POST','DELETE','PUT'],
+    credentials:true,
+}));
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended:true}));
 
-app.use(
-  fileUpload({
-    useTempFiles: true,
-    tempFileDir: "/temp",
-  })
-);
+app.use(fileUpload({
+    useTempFiles:true,
+    tempFileDir:"/temp",
+}));
 
-app.use("/user", userRouter);
-app.use("/application", applicationRouter);
-app.use("/job", jobRouter);
+app.use("/user",userRouter);
+app.use("/application",applicationRouter);
+app.use("/job",jobRouter);
 
-// Database connection
+//database
 db();
 
-// Error middleware should be placed at the end
+//error middleware in last
 app.use(errorMiddleware);
 
 export default app;
--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 import app from "./app.js";
-import cloudinary from "cloudinary";
+import cloudinary from 'cloudinary';
 
-// Configure Cloudinary
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
-  api_key: process.env.CLOUDINARY_CLIENT_API,
-  api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
+  api_key:process.env.CLOUDINARY_CLIENT_API,
+  api_secret: process.env.CLOUDINARY_CLIENT_SECRET
 });
 
-// Use environment variable for port
 const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(4000,() => {
+  console.log(`server is running on port ${process.env.PORT}`);
 });
