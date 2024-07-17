5aec23273ed6909f291791dadabc964d05074bdd
Ar273404 <arun273404@gmail.com>
2024-07-17 20:06:54 +0530

    jh

--- app.js
+++ app.js
@@ -1,52 +1,49 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import fileUpload from "express-fileupload";
-import userRouter from "./routes/userRouter.js";
-import jobRouter from "./routes/jobRouter.js";
-import applicationRouter from "./routes/applicationRouter.js";
-import { db } from "./database/db.js";
-import { errorMiddleware } from "./middlewares/error.js";
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import fileUpload from 'express-fileupload';
+import userRouter from './routes/userRouter.js'
+import jobRouter from './routes/jobRouter.js'
+import applicationRouter from './routes/applicationRouter.js'
+import { db } from './database/db.js';
+import { errorMiddleware } from './middlewares/error.js';
+
 
 const app = express();
 
-dotenv.config({ path: "./config/config.env" });
+dotenv.config({path:'./config/config.env'});
+
+
 
 // Define a route for '/'
-app.get("/", (req, res) => {
-  res.send("Hello, World!"); // Respond with a simple message
+app.get('/', (req, res) => {
+  res.send('Hello, World!'); // Respond with a simple message
 });
 
-// CORS configuration
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL, // Ensure this matches exactly with your frontend URL
-    methods: ["GET", "POST", "DELETE", "PUT", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
-);
 
+app.use(cors({
+    origin:[process.env.FRONTED_URL],
+    methods:['GET','POST','DELETE','PUT','OPTIONS'],
+    credentials:true,
+}));
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended:true}));
 
-app.use(
-  fileUpload({
-    useTempFiles: true,
-    tempFileDir: "/temp",
-  })
-);
+app.use(fileUpload({
+    useTempFiles:true,
+    tempFileDir:"/temp",
+}));
 
-app.use("/user", userRouter);
-app.use("/application", applicationRouter);
-app.use("/job", jobRouter);
+app.use("/user",userRouter);
+app.use("/application",applicationRouter);
+app.use("/job",jobRouter);
 
-// Database connection
+//database
 db();
 
-// Error middleware at the end
+//error middleware in last
 app.use(errorMiddleware);
 
 export default app;
